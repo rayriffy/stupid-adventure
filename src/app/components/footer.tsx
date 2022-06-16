55054594f1d5533ae230f1ba0a27c35e9b0af3c1
Phumrapee Limpianchop <git@rayriffy.com>
2022-06-17 05:38:19 +0700

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,7 +3,7 @@ import { Component } from 'solid-js'
 export const Footer: Component = () => {
   return (
     <footer class="mt-6 text-gray-400">
-      <p class="text-center text-xs sm:text-sm">&copy; 2022 rayriffy</p>
+      <p class="text-center text-xs sm:text-sm">&copy; 2022 Phumrapee Limpianchop</p>
     </footer>
   )
 }
